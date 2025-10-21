bdfe481b40a6a169d148decb1b37ce12bebbdb4a
Gabs-jg <gabriel19joao@gmail.com>
2025-10-21 16:16:18 -0300

    modificação database.py

--- Documentos/Sprint 7/QAx/projects/mark85-robot-express/resources/libs/database.py
+++ Documentos/Sprint 7/QAx/projects/mark85-robot-express/resources/libs/database.py
@@ -2,7 +2,7 @@ from robot.api.deco import keyword
 from pymongo import MongoClient
 import bcrypt
 
-client = MongoClient('mongodb+srv://qa:xperience@cluster0.b4jsvse.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+client = MongoClient('')
 
 db = client['markdb']
 
